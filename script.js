113871972e6fc22333982b1015d48568e2023ca4
Sudhakar Kumar <sudhakarsahu900@gmail.com>
2026-06-10 19:41:17 +0530

    Initial GRMS Project

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("loginForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let username =
+    document.getElementById("username").value;
+
+    let password =
+    document.getElementById("password").value;
+
+    if(username === "admin" &&
+       password === "admin123")
+    {
+        alert("Login Successful");
+
+        window.location.href =
+        "dashboard.html";
+    }
+    else
+    {
+        alert("Invalid Username or Password");
+    }
+
+});
